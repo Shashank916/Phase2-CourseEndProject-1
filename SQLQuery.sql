aca830d28f0100d389c22ad52ba20a1a4de90cb6
Shashank K S <shashank.s2@mphasis.com>
2023-09-06 13:54:30 +0530

    Course End Project/Project_1

---
+++ SQLQuery.sql
@@ -0,0 +1,9 @@
+use Phase2_end_project
+
+INSERT INTO Employee (EmpCode, EmpName, Email, DateOfBirth, Department_Code)
+VALUES
+    (1, 'John Doe', 'john.doe@example.com', '1990-05-15', 1),
+    (2, 'Jane Smith', 'jane.smith@example.com', '1985-09-22', 2),
+    (3, 'Michael Johnson', 'michael.johnson@example.com', '1988-12-10', 1),
+    (4, 'Emily Brown', 'emily.brown@example.com', '1992-03-28', 3),
+    (5, 'David Wilson', 'david.wilson@example.com', '1995-07-05', 4);
